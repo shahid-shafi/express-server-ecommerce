3849ea8156fdf2ab3ae41ad13cff4b7f2add7190
shahid-shafi <shahidshafi.org@gmail.com>
2023-06-09 22:37:19 +0530

    config - netlify configuration

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,6 @@
 import app from "../src/app";
-export default app;
+import ServerlessHttp from "serverless-http";
+import combinedRouter from "../src/routers";
+
+app.use('/.netlify/functions/api', combinedRouter)
+module.exports.handler = ServerlessHttp(app)
